3742edf1d7a44064dd3d0c79000807a5465484ac
Sean Murphy <Sean Murphy>
2016-06-25 00:08:07 -0400

    miniserver commit

---
+++ miniclient.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2.7
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('192.168.0.12', 8081))
+s.send("Happy Hacking".encode())
+data = s.recv(1024).decode()
+s.close()
+
+print ('Received')
+print (data)
---
+++ miniserver.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import socket
+import sys
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ("192.168.0.12", 8081)
+print 'starting up on %s port %s' % server_address
+server.bind(server_address)
+
+server.listen(5)
+
+connection, client_address = server.accept()
+print 'connection from', connection.getpeername()
+
+data = connection.recv(4096)
+if data
+	print 'received ', repr(data)
+
+	data = data.rstrip()
+	connection.send("%s\n%s\n%s\n" % ('-'*80, data.center(80), '-'*80))
+	print "Response sent!"
+
+connection.shutdown(socket.SHUT_RD | socket.SHUT_WR)
+connection.close
+print "Connection closed."
+
+server.close()
